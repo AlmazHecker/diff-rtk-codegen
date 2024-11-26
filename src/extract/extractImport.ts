686a50e9f9fa81118e22bf5f099b0bdb8dab1804
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2024-11-26 20:58:35 +0600

    fix(function args): fix issue related to function args

--- src/extract/extractImport.ts
+++
@@ -1,9 +0,0 @@
-import * as ts from "typescript";
-
-/**
- * Extracts import statements from a given node.
- * This will return an array of objects representing the import statements.
- */
-export const extractImport = (node: ts.Node): string => {
-  return node.getText();
-};
